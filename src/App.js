8b9c1853ee04c42a48ba523a13d7ac141878e530
Akash <mr.shetty28502@gmail.com>
2023-08-09 18:06:31 +0530

    Added ‘Delete’ button

--- src/App.js
+++ src/App.js
@@ -3,18 +3,45 @@ import './App.css';
 import Header from './Header';
 
 function App() {
+  let sub = [
+    {
+      id: 1,
+      name: 'Akash',
+      phone: '1234567890'
+    },
+    {
+      id: 2,
+      name: 'V Akash',
+      phone: '8521234567'
+    }
+  ]
   return (
     <div className='card'>
-      <Header/>
+      <Header />
       <div className='content'>
-      <button className='button'>Add</button>
-      <div className='card-content heading-container '>
-        <span className='name content-info'>Name</span><br/>
-        <span className='number content-info'>Phone</span>
-      </div>
+        <button className='button'>Add</button>
+        <div className='card-content heading-container '>
+          <span className="grid-item name-heading">Name</span>
+          <span className="grid-item phone-heading">Phone</span>
+        </div>
+        {
+          sub.map(sub => {
+            return (
+              <div className='card-content' key={sub.id}>
+                
+                <span className="grid-item">{sub.name}</span>
+                <span className="grid-item">{sub.phone}</span>
+                
+                <span className="grid-item action-btn-container">
+                  <button className="custom-btn delete-btn">Delete</button>
+                </span>
+              </div>
+            )
+          })
+        }
       </div>
     </div>
-   );
+  );
 }
 
 export default App;
